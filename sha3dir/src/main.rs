f5344fffd98f0d1c8c2c3144de26fd5f616e2eb8
ashtneoi <ashtneoi@gmail.com>
2021-02-15 23:57:48 -0800

    Attempt to print SHA3-256 hash of dir

--- sha3dir/src/main.rs
+++ sha3dir/src/main.rs
@@ -1,11 +1,15 @@
+use sha3::{Digest, Sha3_256};
 use std::collections::VecDeque;
 use std::env::args;
-use std::fs::read_dir;
+use std::fs::{File, read_dir};
+use std::io::{self, prelude::*};
 use std::path::PathBuf;
+use std::os::unix::ffi::OsStrExt;
 
 fn main() {
     let argv: Vec<_> = args().collect();
     assert_eq!(argv.len(), 2);
+    let mut hasher = Sha3_256::new();
     let mut queue = VecDeque::new();
     queue.push_back(PathBuf::from(&argv[1]));
     while !queue.is_empty() {
@@ -25,12 +29,33 @@ fn main() {
 
         for entry in entries {
             let file_type = entry.1.file_type().unwrap();
+            let len = entry.1.metadata().unwrap().len();
+            hasher.update(&len.to_le_bytes());
+            let path = entry.1.path();
+            hasher.update(path.as_os_str().as_bytes());
             if file_type.is_file() {
+                let mut f = File::open(&path).unwrap();
+                const BLOCK_SIZE: usize = 8192;
+                let mut buf = vec![0_u8; BLOCK_SIZE];
+                loop {
+                    match f.read(&mut buf) {
+                        Ok(0) => break,
+                        Ok(count) => hasher.update(&buf[..count]),
+                        Err(e) => {
+                            if e.kind() != io::ErrorKind::Interrupted {
+                                panic!("{:?}", e);
+                            }
+                        },
+                    }
+                }
                 println!("{:?}", entry.0);
             } else if file_type.is_dir() {
-                println!("{:?}/", entry.0);
-                queue.push_back(entry.1.path());
+                println!("{:?}/", &entry.0);
+                queue.push_back(path);
             }
+            // TODO: symlinks etc.
         }
     }
+
+    println!("{:?}", hasher.finalize().as_slice());
 }
